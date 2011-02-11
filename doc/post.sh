1204fe81a7972d9580706e808b8b360178f3fbfc
Scott Livingston <slivingston@caltech.edu>
2011-02-11 01:34:41 -0800

    misc additions to reference manual.

---
+++ doc/post.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# copy build of doc to private (temporary) location,
+# http://www.cds.caltech.edu/~slivings/off-site/BatStack_man.pdf
+
+scp BatStack_man.pdf slivings@cds.caltech.edu:/home/users/slivings/public_html/off-site/
+
