9ff90a353f1afb6deb0630d67ec9947e1f6bce61
Scott Livingston <slivingston@caltech.edu>
2011-07-31 14:31:57 -0700

    update doc sync script.

--- doc/rsync-it.sh
+++ doc/rsync-it.sh
@@ -3,5 +3,4 @@
 # copy build of doc to private (temporary) location,
 # http://www.cds.caltech.edu/~slivings/off-site/BatStack_man.pdf
 
-scp BatStack_man.pdf slivings@cds.caltech.edu:/home/users/slivings/public_html/off-site/
-
+rsync -vz BatStack_man.pdf slivings@www.cds.caltech.edu:/home/slivings/public_html/off-site/
